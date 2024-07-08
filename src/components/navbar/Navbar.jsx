ce1a627b03bc96001117af01f83ca30e90419f8b
Aashish Pandey <pandey786aashish@gmail.com>
2024-07-08 15:19:16 -0400

    in this commit we will complete crate responsive website from mobile and desktop

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,14 +3,27 @@ import './Navbar.css';
 import logo from '../../assets/logo.png';
 import under_img from '../../assets/nav_img.svg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import toggle_img from '../../assets/toggle.svg';
+import menu_close from '../../assets/menuclose.svg';
+import { useRef } from 'react';
 
 const Navbar = () => {
   const [menu, setMenu] = useState('');
+  const menuRef=useRef();
 
+   const openMenu=()=>{
+    menuRef.current.style.right="0"
+   }
+   const closeMenu=()=>{
+    menuRef.current.style.right="-350px"
+   }
   return (
     <div className='navbar'>
       <img src={logo} alt="" className='logo' />
-      <ul className='nav-menu'>
+      <img src={toggle_img} onClick={openMenu} alt="" className='nav-mob-open'/>
+      
+      <ul ref={menuRef} className='nav-menu'>
+        <img src={menu_close} onClick={closeMenu} alt="" className="nav-mob-close"/>
         <li>
           <AnchorLink className='anchor-link' href="#home">
           <p onClick={() => setMenu('home')}>Home</p>
@@ -42,6 +55,7 @@ const Navbar = () => {
           {menu === 'contact' && <img src={under_img} alt='' />}
         </li>
       </ul>
+
       <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href="#contact">Connect With Me</AnchorLink></div>
     </div>
   );
